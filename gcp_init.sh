b21c92a1ae2abe2f53dfa35934477ea196f665f8
z9gmoon <z9gcaptcha@gmail.com>
2021-10-03 01:03:53 +0700

    Update gcp_init.sh

--- gcp_init.sh
+++ gcp_init.sh
@@ -1,3 +1,8 @@
 sudo adduser moon
 sudo usermod -a -G root moon
 sudo vim /etc/ssh/sshd_config
+
+# https://github.com/sivel/speedtest-cli
+# https://support.strongvpn.com/hc/en-us/articles/360038982774-How-to-Change-DNS-in-Linux
+sudo nano /etc/resolv.conf
+nameserver 198.18.0.1
